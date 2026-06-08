6c0c6255a171cdaba0053adfe116c043667e7fe5
prakharsharma13 <prakharsharma752@gmail.com>
2026-06-08 23:35:38 +0530

    remove outermost parenthesis in strings

---
+++ Sorting.js/removed-outermost-parenthesis.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var removeOuterParentheses = function (s) {
+  let depth = 0;
+  let ans = "";
+
+  for (let ch of s) {
+    if (ch === "(") {
+      if (depth > 0) {
+        ans += ch;
+      }
+      depth++;
+    } else {
+      depth--;
+      if (depth > 0) {
+        ans += ch;
+      }
+    }
+  }
+  return ans;
+};
